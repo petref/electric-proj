0ce0a0219bed41b21db31a494cf39d1c2d8c3445
petref <petroupb@gmail.com>
2024-08-02 17:00:49 +0300

    CleanUp & Format

--- services/api/index.ts
+++ services/api/index.ts
@@ -7,13 +7,29 @@ const api = axios.create({
 export interface Region {
     id: string;
     name: string;
-    currentPrice: number;
+    currentPrice: string | string[];
     unit?: string;
 }
 
 interface RegionResponse {
     regions: Region[];
     nextPage?: number;
+}
+
+interface RegionDetail {
+  unix_seconds:string[],
+  price: string[];
+  name: string;
+}
+
+interface RegionPublicPower{
+  unix_seconds:string[],
+  production_types: ProductionType[]
+};
+
+interface ProductionType {
+  name: string;
+  data: string[];
 }
 
 const extractRegions = (htmlString: string): Region[] => {
@@ -25,7 +41,7 @@ const extractRegions = (htmlString: string): Region[] => {
       const regionNames = match[2];
       const regionId = match[0].split(" ")[0].trim();
         if (regionNames) {
-          regions.push({ id: regionId, name:regionNames, currentPrice: Math.floor(Math.random() * 100,)});
+          regions.push({ id: regionId, name:regionNames, currentPrice: `{Math.floor(Math.random() * 100,)}`});
         }
     }
     return regions;
@@ -54,5 +70,20 @@ export default {
     
         const regionsWithPrices = await Promise.all(regionPricePromises);
         return { regions: regionsWithPrices };
+    },
+
+    async getRegionPrices(regionId: string): Promise<RegionDetail> {
+      const response = await api.get(`/price?bzn=${regionId}`);
+      const data = await response.data;
+      return response.data;
+    },
+    async getPublicPower (regionId: string): Promise<RegionPublicPower[]> {
+      const region = regionId.split("-")
+      console.log(region)
+      const response = region.map(async (regEl) => await api.get(`/public_power?country=${region[0].toLowerCase()}`))
+      const regions = await Promise.all(response);
+      const data = regions.map((el) => el.data);
+      console.log(data)
+      return data;
     }
 }
