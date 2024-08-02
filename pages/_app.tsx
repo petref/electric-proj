b916cbb41a911e099b2ae5c8892420f444b3c2b0
petref <petroupb@gmail.com>
2024-08-02 15:46:26 +0300

    Add react-query context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import type { AppProps } from 'next/app';
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
